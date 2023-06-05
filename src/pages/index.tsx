2db57d4d3eb09dfaab799a53b7cc3fa90c5e94bf
WhiteDog <dlrlgus1204@naver.com>
2023-06-05 11:33:11 +0900

    OS-12 feat, 파비콘, 타이틀 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import { DraggableDatas } from "@/components/Draggable";
 const Home = () => {
 	return (
 		<div className="App">
+			<title>OmegaStrikers</title>
 			<DraggableDatas />
+			<link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
 		</div>
 	);
 };
